62836e6c7d89b3b8db660efb3e65159736145b36
MiguelGuerrero07 <miguel.magr0202@gmail.com>
2022-05-19 14:12:26 -0500

    Se hacen cambios en las carpetas 1 , 2 ,3

---
+++ 03-Strings/js/02-app.js
@@ -0,0 +1,11 @@
+//Metodos para cadenas de Texto.
+const producto ='Monitor 20 pulgadas"';
+
+console.log(producto);
+
+//Saber el núimero de caracteres de nuestra cadena de texto
+console.log(producto.length); 
+
+//console.log(producto.indexOf('Monitor'));
+
+console.log(producto.includes('Monitor'));
