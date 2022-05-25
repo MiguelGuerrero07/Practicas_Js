81ae6b4e3cf8fd5ff2d75feb6d2bede173b0180a
MiguelGuerrero07 <miguel.magr0202@gmail.com>
2022-05-25 15:29:36 -0500

    Cambios en los index de las carpetas 3,4,5,6

---
+++ 06-Booleans/js/03-app.js
@@ -0,0 +1,11 @@
+const autenticado = true;
+
+if(autenticado === true){
+    console.log('Si puedes verlo');
+}else{
+    console.log('No lo puedes ver');
+}
+
+//Operador ternario
+
+console.log(autenticado ? 'Si esta autenticado' : 'No esta auntenticado');
