0b81c775ef9dcd22e40193d2e344765268772479
Miguel Guerrero <miguel0202guerrero@gmail.com>
2022-06-08 11:48:14 -0500

    Proyecto Finalizado , aun hay impresiosiones en algunas partes del codigo

--- 16-PROYECTO-EnviarEmail/js/app.js
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -1,11 +1,14 @@
 //Varibales
 const btnEnviar = document.querySelector('#enviar');
+const btnReset = document.querySelector('#resetBtn');
 const formulario = document.querySelector('#enviar-mail');
 
 //Varibales para campso
 const email = document.querySelector('#email');
 const asunto = document.querySelector('#asunto');
 const mensaje = document.querySelector('#mensaje');
+const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        
 
 eventListeners();
 function eventListeners() {
@@ -16,13 +19,24 @@ function eventListeners() {
     email.addEventListener('blur',validarFormulario);
     asunto.addEventListener('blur',validarFormulario);
     mensaje.addEventListener('blur',validarFormulario);
+
+    //Reinicia el form
+    btnReset.addEventListener('button',resetearFormulario);
+
+    //ENVIAR EMAIL 
+    formulario.addEventListener('submit',enviarEmail);
+
+
+
+
+
 }
 
 
 //fUNCIONES
 function iniciarApp(){
     btnEnviar.disable = true;
-    btnEnviar.classList.add('cursor-not-all-allowed','opacity-50')
+    btnEnviar.classList.remove('cursor-not-all-allowed','opacity-50')
 }
 
 //Valdar formulario
@@ -30,9 +44,10 @@ function validarFormulario(e){
 
     if(e.target.value > 0){
         
-        // Elimina los errores
         const error = document.querySelector('p.error');
-        error.remove();
+        if(error){
+            error.remove();
+        }
 
         e.target.classList.remove('border','border-red-500');
         e.target.classList.add('border','border-green-500');
@@ -44,11 +59,14 @@ function validarFormulario(e){
     }
 
     if(e.target.type === 'email'){
-        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
         
         if(er.test(e.target.value))  {
+            //Elimina los errores
             const error = document.querySelector('p.error');
-            error.remove();
+            if(error){
+                error.remove();
+            }
     
             e.target.classList.remove('border','border-red-500');
             e.target.classList.add('border','border-green-500');
@@ -58,6 +76,11 @@ function validarFormulario(e){
             e.target.classList.add('border','border-red-500');
             mostrarError('EMAIL NO VALIDO');
         }   
+    }
+
+    if(er.test(email.value)  && asunto.value !=='' && mensaje.value !==''){
+        btnEnviar.disable = false;
+        btnEnviar.classList.remove('cursor-not-all-allowed','opacity-50')
     }
 }
 
@@ -71,4 +94,41 @@ function mostrarError(mensaje){
         formulario.appendChild(mensajeError);
     }
 
+}
+
+
+//Enviar el email
+function enviarEmail(e){
+    e.preventDefault();
+
+    //Mostrar Spiner
+    const spinner = document.querySelector('#spinner');
+    spinner.style.display= 'flex';
+
+    //Despues de 3 segundos ocultar el spinner y mostrar el mensaje
+    setTimeout(() => {
+        spinner.style.display = 'none'
+
+
+        //Mensaje que dice que se envio correctamente
+        const parrafo = document.createElement('p');
+        parrafo.textContent = 'El mensaje se envio correctamente';
+        parrafo.classList.add('text-center','my-10','p-2','bg-green-500', 'text-white','font-bold','uppercase')
+
+        //Inserta el parrafo antes del spiner
+        formulario.insertBefore(parrafo,spinner);
+        
+        setTimeout(()=>{
+            parrafo.remove(); //Elimina el mensaje de enviado
+            resetearFormulario();
+        },5000);
+    } ,3000);
+    
+}
+
+//Fuction de resear el formulario
+
+function resetearFormulario(){
+    formulario.reset();
+    iniciarApp();
 }
