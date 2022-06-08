4099832cf3a639be3b0e25098530dc97b728d2d1
Miguel Guerrero <miguel0202guerrero@gmail.com>
2022-06-08 09:21:36 -0500

    Cambios en el Js

---
+++ 16-PROYECTO-EnviarEmail/js/app.js
@@ -0,0 +1,74 @@
+//Varibales
+const btnEnviar = document.querySelector('#enviar');
+const formulario = document.querySelector('#enviar-mail');
+
+//Varibales para campso
+const email = document.querySelector('#email');
+const asunto = document.querySelector('#asunto');
+const mensaje = document.querySelector('#mensaje');
+
+eventListeners();
+function eventListeners() {
+    //Cuando la app arranca
+    document.addEventListener('DOMContentLoaded',iniciarApp);
+
+    //Campos del formulario
+    email.addEventListener('blur',validarFormulario);
+    asunto.addEventListener('blur',validarFormulario);
+    mensaje.addEventListener('blur',validarFormulario);
+}
+
+
+//fUNCIONES
+function iniciarApp(){
+    btnEnviar.disable = true;
+    btnEnviar.classList.add('cursor-not-all-allowed','opacity-50')
+}
+
+//Valdar formulario
+function validarFormulario(e){
+
+    if(e.target.value > 0){
+        
+        // Elimina los errores
+        const error = document.querySelector('p.error');
+        error.remove();
+
+        e.target.classList.remove('border','border-red-500');
+        e.target.classList.add('border','border-green-500');
+
+    }else{
+        e.target.classList.remove('border','border-green-500');
+        e.target.classList.add('border','border-red-500');
+        mostrarError('Todos los campos son obligatorios');
+    }
+
+    if(e.target.type === 'email'){
+        const er = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        
+        if(er.test(e.target.value))  {
+            const error = document.querySelector('p.error');
+            error.remove();
+    
+            e.target.classList.remove('border','border-red-500');
+            e.target.classList.add('border','border-green-500');
+    
+        }else{
+            e.target.classList.remove('border','border-green-500');
+            e.target.classList.add('border','border-red-500');
+            mostrarError('EMAIL NO VALIDO');
+        }   
+    }
+}
+
+function mostrarError(mensaje){
+    const mensajeError = document.createElement('p');
+    mensajeError.textContent = mensaje;
+    mensajeError.classList.add('border','border-red-500','background-red-30','text-red-500','p-3','mt-5','text-center','error');
+
+    const errores = document.querySelectorAll('.error');
+    if(errores.length === 0){
+        formulario.appendChild(mensajeError);
+    }
+
+}
